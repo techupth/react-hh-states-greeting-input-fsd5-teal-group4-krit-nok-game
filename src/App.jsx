a6731c45a52bca05eab0b83040a82ee7584e8dd7
Nok <siriwan.skk@gmail.com>
2023-12-21 14:22:24 +0700

    feat: finish ex1-2 states input

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import "./App.css";
+import GreetingMessageEx1 from "./components/GreetingMessageEx1";
+import GreetingMessageEx2 from "./components/GreetingMessageEx2";
 
 function App() {
   return (
-    <div className="App">
-      <div className="greeting-container">Greeting Message</div>
-      <div className="input-container">
-        <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
-      </div>
-
-      <div className="buttons">
-        <button>Update text</button>
-      </div>
-    </div>
+    <>
+      <GreetingMessageEx1 />
+      <br />
+      <GreetingMessageEx2 />
+    </>
   );
 }
 
